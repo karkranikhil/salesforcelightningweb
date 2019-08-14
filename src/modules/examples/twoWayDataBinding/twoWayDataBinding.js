0c7dd5bbfc9a69af10f9badb8b0dc332295ab819
nikhil karkra <karkra.nikhil@gmail.com>
2019-08-14 22:24:04 +1000

    Two way binding

--- src/modules/examples/twoWayDataBinding/twoWayDataBinding.js
+++ src/modules/examples/twoWayDataBinding/twoWayDataBinding.js
@@ -3,7 +3,7 @@ export default class TwoWayDataBinding extends LightningElement {
     @track name = 'Nikhil karkra';
     @track title = 'Salesforce developer';
 
-    changeHandler(event){
-        this[event.target.name] = event.target.value
+    changeHandler(event) {
+        this[event.target.name] = event.target.value;
     }
 }
